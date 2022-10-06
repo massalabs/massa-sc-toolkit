d9b9cb6500b05d93cbcdd62e1ee546417599ebbc
Grégory Libert <Grgory Libert>
2022-10-06 14:49:45 +0200

    fixes path

--- initialize.js
+++ initialize.js
@@ -2,26 +2,27 @@
 
 import { execSync } from 'child_process';
 import * as fs from 'fs';
+import * as path from 'path';
 
 const DEV_DEPENDENCIES = ["assemblyscript", "@massalabs/massa-as-sdk"];
 
 export function initialize(directory) {
     console.log("Installation begun...");
 
-    if (fs.existsSync(dir)) {
-        console.error(`Project directory {dir} already exist. Please chose another project name`);
+    if (fs.existsSync(directory)) {
+        console.error(`Project directory ${directory} already exist. Please chose another project name.`);
         return;
     }
 
-    fs.mkdirSync(dir);
+    fs.mkdirSync(directory);
 
-    const option = { cwd: path.join(process.cwd(), dir) };
+    const option = { cwd: path.join(process.cwd(), directory) };
 
     execSync('npm init -y', option);
     execSync('npm install --save-dev ' + DEV_DEPENDENCIES.join(' '), option);
     execSync('npx asinit . -y', option);
-    fs.rmSync('./index.html', option);
-    fs.rmSync('./tests', { recursive: true, force: true, ...option });
+    fs.rmSync(path.join(process.cwd(), directory, 'index.html'));
+    fs.rmSync(path.join(process.cwd(), directory, 'tests'), { recursive: true, force: true });
 
     console.log("Installation successfully completed");
 }
