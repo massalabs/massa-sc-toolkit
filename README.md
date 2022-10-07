b32f55db105fb8a152ad9f7ecbac6187ee9df827
ASAPSegfault <45430752+ASAPSegfault@users.noreply.github.com>
2022-10-07 17:04:36 +0200

    4 initialize a new project (#13)
    adds initialize command.
    
    Co-authored-by: Grégory Libert <Grégory Libert>

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # massa-sc-toolkit
+
+This toolkit is meant to facilitate smart contract development.
+
+> __PREREQUISITES:__ NPM installed on your computer
+
+## Repository Initialisation
+
+Simply run the following command:
+
+```shell
+npx github:massalabs/massa-sc-toolkit init <projectName>
+```
+
+You now have your own AssemblyScript project setup, with Massa's sdk installed.
+
+You can now run `npm run asbuild` to compile your AssemblyScript files.
---
+++ initialize.js
@@ -0,0 +1,28 @@
+"use strict";
+
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const DEV_DEPENDENCIES = ["assemblyscript", "@massalabs/massa-as-sdk"];
+
+export function initialize(directory) {
+    console.log("Installation begun...");
+
+    if (fs.existsSync(directory)) {
+        console.error(`Project directory ${directory} already exist. Please chose another project name.`);
+        return;
+    }
+
+    fs.mkdirSync(directory);
+
+    const option = { cwd: path.join(process.cwd(), directory) };
+
+    execSync('npm init -y', option);
+    execSync('npm install --save-dev ' + DEV_DEPENDENCIES.join(' '), option);
+    execSync('npx asinit . -y', option);
+    fs.rmSync(path.join(process.cwd(), directory, 'index.html'));
+    fs.rmSync(path.join(process.cwd(), directory, 'tests'), { recursive: true, force: true });
+
+    console.log("Installation successfully completed");
+}
