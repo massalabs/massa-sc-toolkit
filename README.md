445385928cc55746e2264303e63558490d441ade
Sebastien Valla <83454858+SebastienValla@users.noreply.github.com>
2022-10-10 14:09:47 +0200

    9 generate webmodule (#14)
    adds webmodule generation helper/documentation.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This toolkit is meant to facilitate smart contract development.
 
-> __PREREQUISITES:__ NPM installed on your computer
+> **PREREQUISITES:** NPM installed on your computer
 
 ## Repository Initialisation
 
@@ -15,3 +15,22 @@ npx github:massalabs/massa-sc-toolkit init <projectName>
 You now have your own AssemblyScript project setup, with Massa's sdk installed.
 
 You can now run `npm run asbuild` to compile your AssemblyScript files.
+
+To use librairies as massa-as-sdk and @massalabs/as you need to import the required function, for instance :
+
+```jsx
+import { generateEvent } from "@massalabs/massa-as-sdk";
+export function HelloWorld(): void {
+  generateEvent(`Hello World`);
+}
+```
+
+# How to …
+
+## Include another smart contract inside a smart contract ?
+
+You can follow the following documentation :
+
+https://github.com/massalabs/as/tree/main/transformer
+
+###
--- initialize.js
+++ initialize.js
@@ -1,28 +1,38 @@
 "use strict";
 
-import { execSync } from 'child_process';
-import * as fs from 'fs';
-import * as path from 'path';
+import { execSync } from "child_process";
+import * as fs from "fs";
+import * as path from "path";
 
-const DEV_DEPENDENCIES = ["assemblyscript", "@massalabs/massa-as-sdk"];
+const DEV_DEPENDENCIES = [
+  "assemblyscript",
+  "@massalabs/massa-as-sdk",
+  "@massalabs/as/assembly",
+  "https://gitpkg.now.sh/massalabs/as/transformer?main",
+];
 
 export function initialize(directory) {
-    console.log("Installation begun...");
+  console.log("Installation begun...");
 
-    if (fs.existsSync(directory)) {
-        console.error(`Project directory ${directory} already exist. Please chose another project name.`);
-        return;
-    }
+  if (fs.existsSync(directory)) {
+    console.error(
+      `Project directory ${directory} already exist. Please chose another project name.`
+    );
+    return;
+  }
 
-    fs.mkdirSync(directory);
+  fs.mkdirSync(directory);
 
-    const option = { cwd: path.join(process.cwd(), directory) };
+  const option = { cwd: path.join(process.cwd(), directory) };
 
-    execSync('npm init -y', option);
-    execSync('npm install --save-dev ' + DEV_DEPENDENCIES.join(' '), option);
-    execSync('npx asinit . -y', option);
-    fs.rmSync(path.join(process.cwd(), directory, 'index.html'));
-    fs.rmSync(path.join(process.cwd(), directory, 'tests'), { recursive: true, force: true });
+  execSync("npm init -y", option);
+  execSync("npm install --save-dev " + DEV_DEPENDENCIES.join(" "), option);
+  execSync("npx asinit . -y", option);
+  fs.rmSync(path.join(process.cwd(), directory, "index.html"));
+  fs.rmSync(path.join(process.cwd(), directory, "tests"), {
+    recursive: true,
+    force: true,
+  });
 
-    console.log("Installation successfully completed");
+  console.log("Installation successfully completed");
 }
