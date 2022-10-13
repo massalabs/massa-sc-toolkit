a7ef90168ca4d82d95bec13cabad54c2b04f0b96
Trottoir <31350013+Trottoir@users.noreply.github.com>
2022-10-13 17:32:55 +0200

    2 Testing & running simulator (#18)
    adds simulator
    
    Co-authored-by: ASAPSegfault <45430752+ASAPSegfault@users.noreply.github.com>

--- README.md
+++ README.md
@@ -19,9 +19,9 @@ You can now run `npm run asbuild` to compile your AssemblyScript files.
 To use librairies as massa-as-sdk and @massalabs/as you need to import the required function, for instance :
 
 ```jsx
-import { generateEvent } from "@massalabs/massa-as-sdk";
+import { generateEvent } from '@massalabs/massa-as-sdk';
 export function HelloWorld(): void {
-    generateEvent(`Hello World`);
+	generateEvent(`Hello World`);
 }
 ```
 
@@ -33,6 +33,42 @@ You can follow the following documentation :
 
 https://github.com/massalabs/as/tree/main/transformer
 
+## ... use the simulator
+
+**_The simulator_** (massa-sc-tester https://github.com/massalabs/massa-sc-tester) mimics the behavior at ledger level of the Massa Blockchain.
+It can also handle smart contracts deployment & smart contract calls. It means that all storage & coin balances modification are taken into account
+.
+It provides :
+
+-   A mocked ledger => `ledger.json` :
+
+    -   contains by **_address_** : **_coin balances_** , **_bytecode contents_** and **_datastore_**
+    -   can be initialized by any mock
+    -   will be modified after execution
+
+-   An execution `.json` ("execution.config.json") file :
+
+    -   Consumed by the `massa-sc-tester.exe`
+    -   List all steps to be executed by the simulator (full example at https://github.com/massalabs/massa-sc-tester/blob/main/execution_config.json to know all examples )
+    -   Can read & execute `.wasm` smart contracts
+
+-   A `trace.json` file overriden at each execution :
+
+    -   Log smart contract events
+    -   Log transaction information
+
+    To run the steps detailed in the `execution_config.json` :
+
+The example already set up can be run with :
+
+-   Compiling the example smart contracts
+    ```
+    $ npm run build
+    ```
+-   Running the simulate script
+    ```
+    $ npm run simulate
+    ```
 ### ... use a linter
 
 There is no specific, well-maintained Assemblyscript linter in the ecosystem.
--- initialize.js
+++ initialize.js
@@ -3,20 +3,22 @@
 import { execSync } from "child_process";
 import * as fs from "fs";
 import * as path from "path";
+import { indexFile } from "./contracts/index.js";
+import { mainFile } from "./contracts/main.js";
 
 const DEV_DEPENDENCIES = [
     "assemblyscript",
     "@massalabs/massa-as-sdk",
-    "eslint",
+    "@massalabs/as",
+    "https://gitpkg.now.sh/massalabs/as/transformer?main",
+    "https://github.com/massalabs/massa-sc-toolkit#2-testing-smart-contract-simulator",
     "@typescript-eslint/eslint-plugin@latest",
     "@typescript-eslint/parser@latest",
     "eslint@latest",
-    "@massalabs/as/assembly",
-    "https://gitpkg.now.sh/massalabs/as/transformer?main",
     "@massalabs/massa-web3",
     "@types/node",
     "dotenv",
-    "tslib"
+    "tslib",
 ];
 
 export function initialize(directory) {
@@ -36,27 +38,57 @@ export function initialize(directory) {
     execSync("npm init -y", option);
     execSync("npm install --save-dev " + DEV_DEPENDENCIES.join(" "), option);
     execSync("npx asinit . -y", option);
+
+    console.log("Packages installed");
+
+    fs.rmSync(path.join(process.cwd(), directory, "assembly/index.ts"));
     fs.rmSync(path.join(process.cwd(), directory, "index.html"));
     fs.rmSync(path.join(process.cwd(), directory, "tests"), {
         recursive: true,
         force: true,
     });
 
-    // Create ESLint & Prettier file
+    // Copy example contract
     fs.writeFileSync(
-        path.join(process.cwd(), directory, ".prettierrc"),
-        fs.readFileSync(".prettierrc")
+        path.join(process.cwd(), directory + "/assembly", "index.ts"),
+        indexFile
+    );
+    fs.writeFileSync(
+        path.join(process.cwd(), directory + "/assembly", "main.ts"),
+        mainFile
     );
 
-    fs.writeFileSync(
-        path.join(process.cwd(), directory, ".eslintrc.json"),
-        fs.readFileSync(".eslintrc.json")
+    // Script creation in packge.json
+    execSync(
+        `cd ${directory} && npx npm-add-script -k "build" -v "asc assembly/index.ts --target release --exportRuntime -o build/index.wasm && asc --transform transformer/file2base64.js assembly/main.ts --target release --exportRuntime -o build/main.wasm" `
+    );
+    execSync(
+        `cd ${directory} && npx npm-add-script -k "simulate" -v "node ./simulator/simulate.js" `
     );
 
-    fs.writeFileSync(
-        path.join(process.cwd(), directory, ".gitignore"),
-        fs.readFileSync(".gitignore")
-    );
+    // Copy Simulator content from massa-sc-toolkit in node_modules
+    fs.mkdirSync(directory + "/simulator");
+    duplicateSimulatorFiles(directory);
+    console.log("Simulator installed");
+
+    execSync(`npm uninstall massa-sc-toolkit`);
 
     console.log("Installation successfully completed");
+}
+
+// Duplicate each file from the node_modules package in the new repo
+function duplicateSimulatorFiles(target) {
+    const nodeModulesPath = "/node_modules/massa-sc-toolkit/simulator/";
+    const files = [
+        "execution_config.json",
+        "massa-sc-tester.exe",
+        "massa-sc-tester",
+        "simulate.js",
+    ];
+    files.forEach((file) => {
+        fs.writeFileSync(
+            path.join(process.cwd(), target + "/simulator", file),
+            fs.readFileSync(target + nodeModulesPath + file)
+        );
+    });
 }
