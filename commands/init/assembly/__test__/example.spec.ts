a58e34f6c35d2ea60b281ed3af8555f1faa239f9
Sebastien Valla <83454858+SebastienValla@users.noreply.github.com>
2022-11-18 17:22:54 +0100

    update examples

--- commands/init/assembly/__test__/example.spec.ts
+++ commands/init/assembly/__test__/example.spec.ts
@@ -1,24 +1,29 @@
 import { event, setStorage } from "../main";
 import { Address, Storage } from "@massalabs/massa-as-sdk";
 
-describe("A group of test", () => {
-    test("A test throwing an error", () => {
+describe("A group of test", (): i32 => {
+    test("A test throwing an error", (): i32 => {
         event();
         const got = 42;
         const want = 41;
         if (got != want) {
             error(got.toString() + ", " + want.toString() + " was expected.");
-            return;
+            return TestResult.Failure;
         }
+        return TestResult.Success;
     });
+    return TestResult.Success;
 });
 
-describe("An other group of test", () => {
-    test("Testing the Storage", () => {
+describe("An other group of test", (): i32 => {
+    test("Testing the Storage", (): i32 => {
         setStorage();
         assert(
             Storage.getOf(new Address("A12E6N5BFAdC2wyiBV6VJjqkWhpz1kLVp2XpbRdSnL1mKjCWT6oR"), "test") == "value",
             "Test failed",
         );
+        return TestResult.Success;
     });
+
+    return TestResult.Success;
 });
