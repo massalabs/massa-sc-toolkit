327cd3e47bf560fdc4e86daaec2ac75834a8c921
Trottoir <maxime.ranciere@2018.icam.fr>
2022-11-30 20:18:52 +0100

    Update deployer for testnet 17

--- commands/init/assembly/main.ts
+++ commands/init/assembly/main.ts
@@ -1,12 +1,17 @@
 // The entry file of your WebAssembly module.
-import { Address, Storage, generateEvent } from "@massalabs/massa-as-sdk";
+import { Address, Storage, generateEvent, toBytes } from "@massalabs/massa-as-sdk";
 
 const testAddress = new Address("A12E6N5BFAdC2wyiBV6VJjqkWhpz1kLVp2XpbRdSnL1mKjCWT6oR");
 
-export function setStorage(): void {
-    Storage.setOf(testAddress, "test", "value");
+const keyTest = toBytes("test");
+const valueTest = toBytes("value");
+
+export function setStorage(_args: StaticArray<u8>): StaticArray<u8> {
+    Storage.setOf(testAddress.toByteString(), keyTest, valueTest);
+    return valueTest;
 }
 
-export function event(): void {
+export function event(_: StaticArray<u8>): StaticArray<u8> {
     generateEvent("I'm an event ");
+    return toBytes("1");
 }
