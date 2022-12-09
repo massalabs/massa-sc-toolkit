ba0177ff22de330943b27a7ca36167dfb6d26785
Nathan Seva <thykof@protonmail.ch>
2022-12-09 11:52:16 +0100

    use massalabs prettier config

--- commands/init/assembly/contracts/run.ts
+++ commands/init/assembly/contracts/run.ts
@@ -1,6 +1,6 @@
 import { Address, Args, call } from '@massalabs/massa-as-sdk';
 
-export function main(_: StaticArray<u8>): StaticArray<u8>  {
+export function main(_: StaticArray<u8>): StaticArray<u8> {
   const address = new Address(
     'A12VkQFUXZ9HSNnGV9jWFHibFJNPMm5yYjFTjiLeQPiqM5vNEwgM',
   );
