8d2ced1c88f9d45a52c736e9b229ba94b6f6ee9a
Nathan Seva <thykof@protonmail.ch>
2022-12-15 09:44:36 +0100

    clea code
    Co-authored-by: Grégory Libert <greg_libert@yahoo.fr>

--- commands/init/assembly/contracts/main.ts
+++ commands/init/assembly/contracts/main.ts
@@ -17,17 +17,17 @@ const valueTestArgs = new Args().add('value');
 const keyTest = toBytes('test');
 const valueTest = toBytes('value');
 
-export function setStorageOf(_args: StaticArray<u8>): StaticArray<u8> {
+export function setStorageOf(_: StaticArray<u8>): StaticArray<u8> {
   Storage.setOf(testAddress, keyTest, valueTest);
   return valueTest;
 }
 
-export function setStorageOfWithArgs(_args: StaticArray<u8>): StaticArray<u8> {
+export function setStorageOfWithArgs(_: StaticArray<u8>): StaticArray<u8> {
   Storage.setOf(testAddress, keyTestArgs, valueTestArgs);
   return valueTestArgs.serialize();
 }
 
-export function setStorage(_args: StaticArray<u8>): StaticArray<u8> {
+export function setStorage(_: StaticArray<u8>): StaticArray<u8> {
   Storage.set(keyTest, valueTest);
   return valueTest;
 }
