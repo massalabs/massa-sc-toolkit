19b7d91986afe369b207bf45749fee368e536d2d
Nathan Seva <thykof@protonmail.ch>
2022-12-15 12:03:47 +0100

    remove template argument

--- commands/init/assembly/__tests__/as-pect-example.spec.ts
+++ commands/init/assembly/__tests__/as-pect-example.spec.ts
@@ -3,40 +3,40 @@ class Vec3 {
 }
 describe('example', () => {
   it('should be 42', () => {
-    expect<i32>(19 + 23).toBe(42, '19 + 23 is 42');
+    expect(19 + 23).toBe(42, '19 + 23 is 42');
   });
 
   it('should be the same reference', () => {
     const ref = new Vec3();
-    expect<Vec3>(ref).toBe(ref, 'Reference Equality');
+    expect(ref).toBe(ref, 'Reference Equality');
   });
 
   it('should perform a memory comparison', () => {
     const a = new Vec3(1, 2, 3);
     const b = new Vec3(1, 2, 3);
 
-    expect<Vec3>(a).toStrictEqual(
+    expect(a).toStrictEqual(
       b,
       'a and b have the same values, (discluding child references)',
     );
   });
 
   it('should compare strings', () => {
-    expect<string>('a=' + '200').toBe('a=200', 'both strings are equal');
+    expect('a=' + '200').toBe('a=200', 'both strings are equal');
   });
 
   it('should compare values', () => {
-    expect<i32>(10).toBeLessThan(200);
-    expect<i32>(1000).toBeGreaterThan(200);
-    expect<i32>(1000).toBeGreaterThanOrEqual(1000);
-    expect<i32>(1000).toBeLessThanOrEqual(1000);
+    expect(10).toBeLessThan(200);
+    expect(1000).toBeGreaterThan(200);
+    expect(1000).toBeGreaterThanOrEqual(1000);
+    expect(1000).toBeLessThanOrEqual(1000);
   });
 
   it('can log some values to the console', () => {
-    log<string>('Hello world!'); // strings!
-    log<f64>(3.1415); // floats!
-    log<u8>(244); // integers!
-    log<u64>(0xffffffff); // long values!
-    log<ArrayBuffer>(new ArrayBuffer(50)); // bytes!
+    log('Hello world!'); // strings!
+    log(3.1415); // floats!
+    log(244); // integers!
+    log(0xffffffff); // long values!
+    log(new ArrayBuffer(50)); // bytes!
   });
 });
