b456f0eac04a2a6b9d43d4192116b76a0b66787f
Nathan Seva <thykof@protonmail.ch>
2022-12-20 13:11:28 +0100

    update boilerplate code

--- commands/init/assembly/contracts/main.ts
+++ commands/init/assembly/contracts/main.ts
@@ -1,38 +1,8 @@
 // The entry file of your WebAssembly module.
-import {
-  Address,
-  Storage,
-  generateEvent,
-  Args,
-  toBytes,
-} from '@massalabs/massa-as-sdk';
-
-const testAddress = new Address(
-  'A12E6N5BFAdC2wyiBV6VJjqkWhpz1kLVp2XpbRdSnL1mKjCWT6oR',
-);
-
-const keyTestArgs = new Args().add('test');
-const valueTestArgs = new Args().add('value');
-
-const keyTest = toBytes('test');
-const valueTest = toBytes('value');
-
-export function setStorageOf(_: StaticArray<u8>): StaticArray<u8> {
-  Storage.setOf(testAddress, keyTest, valueTest);
-  return valueTest;
-}
-
-export function setStorageOfWithArgs(_: StaticArray<u8>): StaticArray<u8> {
-  Storage.setOf(testAddress, keyTestArgs, valueTestArgs);
-  return valueTestArgs.serialize();
-}
-
-export function setStorage(_: StaticArray<u8>): StaticArray<u8> {
-  Storage.set(keyTest, valueTest);
-  return valueTest;
-}
+import { generateEvent, toBytes } from '@massalabs/massa-as-sdk';
 
 export function event(_: StaticArray<u8>): StaticArray<u8> {
-  generateEvent("I'm an event ");
-  return [];
+  const message = "I'm an event!";
+  generateEvent(message);
+  return toBytes(message);
 }
