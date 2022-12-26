1914205a5bf3031f2cc43a54e91d9d9b081aba3e
Nathan Seva <thykof@protonmail.ch>
2022-12-26 17:59:06 +0100

    fix run.ts linting issues

--- commands/init/assembly/contracts/run.ts
+++ commands/init/assembly/contracts/run.ts
@@ -1,7 +1,7 @@
-import { Address, call, callerHasWriteAccess, generateEvent } from '@massalabs/massa-as-sdk';
-import { Args, NoArg } from '@massalabs/as-types';
+import { Address, call, callerHasWriteAccess } from '@massalabs/massa-as-sdk';
+import { NoArg } from '@massalabs/as-types';
 
-export function constructor(args: StaticArray<u8>): StaticArray<u8> {
+export function constructor(_: StaticArray<u8>): StaticArray<u8> {
   // This line is important. It ensure that this function can't be called in the future.
   // If you remove this check someone could call your constructor function and reset your SC.
   if (!callerHasWriteAccess) {
