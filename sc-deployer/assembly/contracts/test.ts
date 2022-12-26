9c5dffca27107ce3eafc830ba1b641adadd84171
Nathan Seva <thykof@protonmail.ch>
2022-12-26 15:17:11 +0100

    move files to create sc-deployer package

--- sc-deployer/assembly/contracts/test.ts
+++
@@ -1,8 +0,0 @@
-import { generateEvent } from '@massalabs/massa-as-sdk';
-import { Args } from '@massalabs/as-types';
-
-export function constructor(args: StaticArray<u8>): StaticArray<u8> {
-  let argsSer = new Args(args);
-  generateEvent(`Hello ${argsSer.nextString().unwrap()}`);
-  return [];
-}
