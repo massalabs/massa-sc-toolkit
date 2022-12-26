66764a36bfb5a0a86c87e19a4ac13340d2f2c9b8
Nathan Seva <thykof@protonmail.ch>
2022-12-26 18:58:08 +0100

    create package for toolkit, update readmes

---
+++ packages/sc-toolkit/README.md
@@ -0,0 +1,21 @@
+# massa-sc-toolkit
+
+This toolkit is meant to facilitate smart contract development.
+
+> **PREREQUISITES:**
+>
+> - NPM installed on your computer
+
+## Repository Initialization
+
+Simply run the following command:
+
+```shell
+npx @massalabs/massa-sc-toolkit init <projectName>
+```
+
+Toolkit documentation is available [here](commands/init/README.md)
+
+## Develop
+
+You can initialize a project with `npx toolkit init testDir`.
---
+++ packages/sc-toolkit/src/initialize.js
@@ -0,0 +1,36 @@
+'use strict';
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import fse from 'fs-extra';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export function initialize(directory) {
+  console.log('Installation begun...');
+
+  if (fs.existsSync(directory)) {
+    console.error(
+      `Project directory ${directory} already exist. Please chose another project name.`,
+    );
+    return;
+  }
+
+  fs.mkdirSync(directory);
+
+  fse.copySync(path.join(dirname(__dirname), 'commands', 'init'), directory);
+
+  execSync('npm install', { cwd: directory });
+
+  // workaround for npm removing .gitignore file when fetching repo...
+  // See: https://github.com/npm/npm/issues/3763
+  fs.renameSync(
+    path.join(directory, 'gitignore'),
+    path.join(directory, '.gitignore'),
+  );
+
+  console.log('Installation successfully completed');
+}
