026b16928fbcd2fbaec9077d8cccb6bef4a61cea
Nathan Seva <thykof@protonmail.ch>
2023-01-04 14:28:43 +0100

    CI/CD: remove debug

--- scripts/publish-dev.sh
+++ scripts/publish-dev.sh
@@ -4,4 +4,4 @@ set -ex
 npm version --ws --preid dev --no-git-tag-version --no-commit-hooks prepatch
 TIME=$(date -u +%Y%m%d%H%M%S)
 sed -i "/version/s/dev.0/dev.$TIME/g" packages/*/package.json
-npm publish --ws --tag dev --access public --dry-run
+npm publish --ws --tag dev --access public
