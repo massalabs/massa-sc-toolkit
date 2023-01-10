cd8d507cd548bb727d4da797abbb4b8c5efa2818
Pierre Seznec <34547263+peterjah@users.noreply.github.com>
2023-01-10 15:18:52 +0100

    check env vars (#92)

--- packages/sc-project-initializer/commands/init/src/deploy.ts
+++ packages/sc-project-initializer/commands/init/src/deploy.ts
@@ -8,14 +8,15 @@ import { Args } from '@massalabs/massa-web3';
 dotenv.config();
 
 const publicApi = process.env.JSON_RPC_URL_PUBLIC;
-
-if (!process.env.WALLET_PRIVATE_KEY || !publicApi) {
+if (!publicApi) {
+  throw new Error('Missing JSON_RPC_URL_PUBLIC in .env file');
+}
+const privKey = process.env.WALLET_PRIVATE_KEY;
+if (!privKey) {
   throw new Error('Missing WALLET_PRIVATE_KEY in .env file');
 }
 
-const deployerAccount = await WalletClient.getAccountFromSecretKey(
-  process.env.WALLET_PRIVATE_KEY,
-);
+const deployerAccount = await WalletClient.getAccountFromSecretKey(privKey);
 
 const __filename = fileURLToPath(import.meta.url);
 
