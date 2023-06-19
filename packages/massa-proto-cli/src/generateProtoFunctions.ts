f1e046b953739fd44440313679d1a3c410d90a45
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-19 14:47:07 +0200

    v1

--- packages/massa-proto-cli/src/generateProtoFunctions.ts
+++ packages/massa-proto-cli/src/generateProtoFunctions.ts
@@ -1,6 +1,6 @@
 import { load } from "protobufjs"; 
 
-export const protoFilePath: string = "./src/proto/";
+export const tempProtoFilePath: string = "./src/proto/";
 export const helperFilePath: string = "./generated";
 
 export interface IProtoFile {
@@ -15,9 +15,19 @@ export interface IProtoFile {
 }
 
 
-export async function getProtoFunctions(protoFileName: string, argPath?: string, resPath?: string): Promise<IProtoFile[]> {
+export async function getProtoFunctions(protoFileContent: string, argPath?: string, resPath?: string): Promise<IProtoFile[]> {
+    
+    // generate a temporary proto file and write the content of protoFileContent in it
+    const protoFileName = "temp.proto";
+    const fs = require('fs');
+    fs.writeFile(tempProtoFilePath + protoFileName, protoFileContent, (err) => {
+        if (err) {
+          console.error('Error writing to file:', err);
+        }
+      });
 
-    const root = await load(protoFilePath + protoFileName);
+    // load the proto file
+    const root = await load(tempProtoFilePath + protoFileName);
     // convert root to JSON
     const json = root.toJSON();
     
@@ -29,8 +39,6 @@ export async function getProtoFunctions(protoFileName: string, argPath?: string,
 
             // check if the function name is the same as the previous one
             if (protoFunctions[protoFunctions.length - 1].funcName != key.slice(0, -7)) {
-                console.log("expected: ", protoFunctions[protoFunctions.length - 1].funcName);
-                console.log("got: ", key.slice(0, -7));
                 throw new Error("Error: {func}RHelper should be placed after {func}Helper in the protoFile.\n Their name do not match. Error at message " + key);
             }
 
@@ -73,6 +81,15 @@ export async function getProtoFunctions(protoFileName: string, argPath?: string,
         }
         
     }
+    // delete the temporary proto file
+    fs.unlink(tempProtoFilePath + protoFileName, (err) => {
+        if (err) {
+            console.error('Error deleting file:', err);
+        }
+    });
+
     return protoFunctions;
 }
 
+// const content = 'syntax = "proto3";message eventHelper {uint64 num = 1;string name = 2;}message eventRHelper { uint64 value = 1;}';
+// const a = getProtoFunctions(content, "argPath", "resPath");
