d70940355564504e0b0fa0a72bf4c3d28ddcae91
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-22 10:30:21 +0200

    fix ci

--- packages/massa-proto-cli/src/ts-gen.ts
+++ packages/massa-proto-cli/src/ts-gen.ts
@@ -1,16 +1,16 @@
-import { execSync } from "child_process";
-import { tempProtoFilePath } from "./protobuf.ts";
+import { execSync } from 'child_process';
 
 /**
  * Generates a helper function for a proto file to allow serialization/deserialization.
- * 
+ *
  * @param protoFile - The path to the proto file.
  * @param helperFilePath - The path to save the helper file.
  */
-export function generateTSHelper(protoFilePath: string, helperFilePath: string): void{
-
-    // generate the helper file using protoc. Throws an error if the command fails.
-    const command = `npx protoc --ts_out="${helperFilePath}" ${protoFilePath}`;
-    execSync(command, {stdio: 'inherit'});
-
+export function generateTSHelper(
+  protoFilePath: string,
+  helperFilePath: string,
+): void {
+  // generate the helper file using protoc. Throws an error if the command fails.
+  const command = `npx protoc --ts_out="${helperFilePath}" ${protoFilePath}`;
+  execSync(command, { stdio: 'inherit' });
 }
