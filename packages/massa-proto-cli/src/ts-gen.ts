e57b48d647b10214ace3fe35f2a735f9d82588f5
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-22 16:40:15 +0200

    Update ts-gen.ts

--- packages/massa-proto-cli/src/ts-gen.ts
+++ packages/massa-proto-cli/src/ts-gen.ts
@@ -1,5 +1,14 @@
 import { ProtoFile } from './protobuf';
 
+/**
+ * Generates the typescript code to import the helper and
+ * the Account interface for the ts Caller
+ *
+ * @param protoFile - The proto file data
+ * @param helperRelativePath - The relative path to the previously generated helper
+ *
+ * @returns The typescript code to import the helper and the Account interface
+ */
 function generateImports(protoFile: ProtoFile, helperRelativePath): string {
   let content = `import { ${protoFile.funcName}Helper } from "${helperRelativePath}";\n\n`;
   content += `import { Account } from 'wallet-provider';`;
