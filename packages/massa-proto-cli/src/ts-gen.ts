3c1b1d371a9c4fc450d1f2b1893fb8cf04c686b2
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-22 16:43:25 +0200

    Update ts-gen.ts

--- packages/massa-proto-cli/src/ts-gen.ts
+++ packages/massa-proto-cli/src/ts-gen.ts
@@ -1,6 +1,12 @@
 import { ProtoFile } from './protobuf';
 import * as returnType from './protoTypes.json';
 
+/**
+ * Generate the typescript function header from the proto file
+ *
+ * @param protoFile - The proto file object
+ * @returns The typescript function header
+ */
 function generateHeader(protoFile: ProtoFile): string {
   let content = `export async function ${protoFile.funcName}(account: Account,`;
   for (let arg of protoFile.argFields) {
