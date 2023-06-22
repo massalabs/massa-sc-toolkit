5ae31c5910e82f1994c2c8cf59fbcad398430564
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-22 16:17:25 +0200

    Update ts-gen.ts

--- packages/massa-proto-cli/src/ts-gen.ts
+++ packages/massa-proto-cli/src/ts-gen.ts
@@ -3,5 +3,8 @@ import { ProtoFile } from './protobuf';
 function generateImports(protoFile: ProtoFile, helperRelativePath): string {
   let content = `import { ${protoFile.funcName}Helper } from "${helperRelativePath}";\n\n`;
   content += `import { Account } from 'wallet-provider';`;
+  content += `export interface ITransactionDetails {
+    operationId: string;
+  }\n`;
   return content;
 }
