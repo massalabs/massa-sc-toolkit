c37f1fa019a36f6d25c9b2b4378ec15ca531e99a
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-22 16:33:29 +0200

    Update ts-gen.ts

--- packages/massa-proto-cli/src/ts-gen.ts
+++ packages/massa-proto-cli/src/ts-gen.ts
@@ -2,21 +2,20 @@ import { ProtoFile } from './protobuf';
 
 /**
  * Generates code to check if unsigned arguments of a protobuf message are negative.
+ *
  * @param {ProtoFile} protoFile - The protofile object.
  * @return {string} The string containing code for unsigned argument checks.
  */
 function generateUnsignedArgCheckCode(protoFile: ProtoFile): string {
+  const unsignedPBTypes = new Set(['uint32', 'uint64', 'fixed32', 'fixed64']);
   let content = '';
   for (let arg of protoFile.argFields) {
-    if (
-      arg.type === 'uint32' ||
-      arg.type === 'uint64' ||
-      arg.type === 'fixed32' ||
-      arg.type === 'fixed64'
-    ) {
-      content += `\tif(${arg.name} < 0){\n`;
-      content += `\t\tthrow new Error("Invalid argument: ${arg.name} cannot be negative");\n`;
-      content += `\t}\n`;
+    if (unsignedPBTypes.has(arg.type)) {
+      content += `
+  if (${arg.name} < 0) {
+    throw new Error("Invalid argument: ${arg.name} cannot be negative accordind to protobuf file.");
+  }
+  `;
     }
   }
   return content;
