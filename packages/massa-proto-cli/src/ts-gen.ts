4225f06f2756b41a7554285f6a7b0e825019e18c
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-22 13:26:34 +0200

    add getResultFromOpID to ts caller

---
+++ packages/massa-proto-cli/src/ts-gen.ts
@@ -0,0 +1,7 @@
+import { ProtoFile } from './protobuf';
+
+function getResultFromOpID(): string {
+  let content = `\t// Retrieve the events and outPuts from the operation ID\n`;
+  // TODO: Retrieve the events and outPuts from the operation ID
+  return content;
+}
