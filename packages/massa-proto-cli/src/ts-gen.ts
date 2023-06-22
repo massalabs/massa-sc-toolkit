a6b23e8655927e6cd3c112acb74fa614ce6dfc54
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-22 13:13:22 +0200

    Update ts-gen.ts

--- packages/massa-proto-cli/src/ts-gen.ts
+++ packages/massa-proto-cli/src/ts-gen.ts
@@ -1,6 +1,6 @@
 import { ProtoFile } from './protobuf';
 
-function generateBody(protoFile: ProtoFile): string {
+function argumentSerialization(protoFile: ProtoFile): string {
   let content = `\t// Serialize the arguments\n`;
   content += `\tconst serializedArgs = ${protoFile.funcName}Helper.toBinary({\n`;
   for (let arg of protoFile.argFields) {
@@ -8,6 +8,5 @@ function generateBody(protoFile: ProtoFile): string {
   }
   content = content.slice(0, -2); // remove the last comma and line break
   content += `\n\t});\n`;
-  content += `\t// Call the Smart Contract and get an operation ID\n`;
   return content;
 }
