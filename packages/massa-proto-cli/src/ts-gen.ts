71fde225067206bea1e9b4321bac9b054fa2b697
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-23 08:49:10 +0200

    Update ts-gen.ts

--- packages/massa-proto-cli/src/ts-gen.ts
+++ packages/massa-proto-cli/src/ts-gen.ts
@@ -1,52 +1,17 @@
-import { ProtoFile } from './protobuf';
-import { writeFileSync } from 'fs';
+import { execSync } from 'child_process';
 
-export function generateTSCaller(
-  outputPath: string,
-  helperRelativePath: string,
-  protoFile: ProtoFile,
-  contractAddress?: string,
-) {
+/**
+ * Generates TypeScript helper class for a given .proto file to allow
+ * serialization/deserialization.
+ *
+ * @param protoFile - The path to the proto file.
+ * @param helperFilePath - The path to save the helper file.
+ */
+export function generateTSHelper(
+  protoFilePath: string,
+  helperFilePath: string,
+): void {
   // generate the helper file using protoc. Throws an error if the command fails.
-  try {
-    generateTSHelper(protoFile.protoPath, outputPath);
-  } catch (e) {
-    throw new Error('Error while generating the helper file: ' + e);
-  }
-
-  let content = '';
-
-  // generate the caller imports
-  content += generateImports(protoFile, helperRelativePath);
-
-  // generate the documentation
-  content += generateDoc(protoFile, contractAddress);
-
-  // generate the caller function header
-  content += generateHeader(protoFile);
-
-  // verify that the given arguments are valid
-  content += generateUnsignedArgCheckCode(protoFile);
-
-  // generate the caller function body
-  content += argumentSerialization(protoFile);
-
-  // send the operation to the blockchain using the provider
-  if (!contractAddress) {
-    contractAddress = 'Paste your contract address here';
-  }
-  content += sendOperation(protoFile, contractAddress);
-
-  // use the operation ID to get the events generated and the output of the sc function
-  content += getResultFromOpID();
-
-  // return the event generated and the output of the sc function
-  content += `\treturn outputData;\n`;
-  content += `}\n`;
-
-  // save content to file
-  const fileName = `${protoFile.funcName}Caller.ts`;
-  console.log('filename: ' + fileName);
-  writeFileSync(outputPath + fileName, content, 'utf8');
-  console.log('Caller file: ' + fileName + ' generated at: ' + outputPath);
+  const command = `npx protoc --ts_out="${helperFilePath}" ${protoFilePath}`;
+  execSync(command, { stdio: 'inherit' });
 }
