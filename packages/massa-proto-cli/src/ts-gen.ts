215e14fdbe482c721e54aa291c9ccae03f600415
Grégory Libert <gl@massa.net>
2023-06-23 09:45:00 +0200

    with npm run fix

--- packages/massa-proto-cli/src/ts-gen.ts
+++ packages/massa-proto-cli/src/ts-gen.ts
@@ -7,13 +7,16 @@ import { ProtoFile } from './protobuf';
  * @param helperRelativePath - The relative path to the previously generated helper
  * @returns The TypeScript code to import the helper and the Account interface
  */
-function generateImports(protoFile: ProtoFile, helperRelativePath: string): string {
+function generateImports(
+  protoFile: ProtoFile,
+  helperRelativePath: string,
+): string {
   return `import { ${protoFile.funcName}Helper } from "${helperRelativePath}";`;
 }
 
 /**
  * Generates the TransactionDetails interface used to get the operation Id returned by a Massa node.
- * @param protoFile 
+ * @param protoFile - The proto file data
  * @returns The TypeScript code that represents the interface.
  */
 function generateTransactionDetailsExport(protoFile: ProtoFile): string {
