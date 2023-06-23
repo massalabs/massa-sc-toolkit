81cdbd7ec09a19ad3e8bfbaeadc2e50057555b8c
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-23 09:03:54 +0200

    Update ts-gen.ts

--- packages/massa-proto-cli/src/ts-gen.ts
+++ packages/massa-proto-cli/src/ts-gen.ts
@@ -7,8 +7,15 @@ import * as returnType from './protoTypes.json';
  * @param protoFile - The proto file object
  * @returns The typescript function header
  */
-function generateHeader(protoFile: ProtoFile): string {
-  let content = `export async function ${protoFile.funcName}(account: Account,`;
+function generateFunctionSignature(protoFile: ProtoFile): string {
+  let content = `export async function ${protoFile.funcName}(coins: bigint,`;
+  content += `${setupArguments(protoFile)})`;
+  content += `: Promise<${protoFile.resType}> {\n`;
+  return content;
+}
+
+function setupArguments(protoFile: ProtoFile): string {
+  let content = '';
   for (let arg of protoFile.argFields) {
     if (!returnType[arg.type]) {
       throw new Error('Invalid type: ' + arg.type);
@@ -16,6 +23,5 @@ function generateHeader(protoFile: ProtoFile): string {
     content += `${arg.name}: ${returnType[arg.type]}, `;
   }
   content = content.slice(0, -2); // remove the last comma and space
-  content += `): Promise<${protoFile.resType}> {\n`;
   return content;
 }
