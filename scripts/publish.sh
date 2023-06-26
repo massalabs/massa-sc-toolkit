441086e791749e93495f47dd492bc3c6dd78ea61
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2023-06-26 16:19:11 +0200

    fix ci release (#243)
    * fix ci release
    
    * remove unused latest

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,14 +4,21 @@ set -e
 npm ci
 npm run build
 
-PACKAGE_NAME=$(cat package.json | jq -r '.name')
-PUBLISH_VERSION=$(cat package.json | jq -r '.version')
-echo "Publishing ${PACKAGE_NAME}@$PUBLISH_VERSION"
+
+for packageDir in packages/*; do
+  if [ -d "$packageDir" ]; then
+    PACKAGE_NAME=$(cat "$packageDir/package.json" | jq -r '.name')
+    PUBLISH_VERSION=$(cat "$packageDir/package.json" | jq -r '.version')
+    echo "Publishing ${PACKAGE_NAME}@${PUBLISH_VERSION}"
+  fi
+done
 
 ref=$1
 TAG=""
 if [[ "$ref" == *"buildnet"* ]]; then
   TAG="--tag buildnet"
+elif [[ "$ref" == *"testnet"* ]]; then
+  TAG="--tag testnet"
 fi
 
-npm publish --access public $TAG
+npm publish --workspace --access public $TAG
