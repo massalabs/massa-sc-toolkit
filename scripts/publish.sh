bc5efc46dc654f0551905964215858d4cd578849
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-06-27 11:22:26 +0200

    244 publish packages separately (#245)
    * Generate powered-by
    
    * publish packages separately
    
    * deploy a specific package
    
    ---------
    
    Co-authored-by: massabot <info@massa.net>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,15 +4,6 @@ set -e
 npm ci
 npm run build
 
-
-for packageDir in packages/*; do
-  if [ -d "$packageDir" ]; then
-    PACKAGE_NAME=$(cat "$packageDir/package.json" | jq -r '.name')
-    PUBLISH_VERSION=$(cat "$packageDir/package.json" | jq -r '.version')
-    echo "Publishing ${PACKAGE_NAME}@${PUBLISH_VERSION}"
-  fi
-done
-
 ref=$1
 TAG=""
 if [[ "$ref" == *"buildnet"* ]]; then
@@ -21,4 +12,20 @@ elif [[ "$ref" == *"testnet"* ]]; then
   TAG="--tag testnet"
 fi
 
-npm publish --workspace --access public $TAG
+for packageDir in packages/*; do
+  if [ -d "$packageDir" ]; then
+    PACKAGE_NAME=$(cat "$packageDir/package.json" | jq -r '.name')
+    PUBLISH_VERSION=$(cat "$packageDir/package.json" | jq -r '.version')
+    
+    # Check if the package name is in the reference
+    if [[ "$ref" == *"$PACKAGE_NAME"* ]]; then
+      echo "Publishing ${PACKAGE_NAME}@${PUBLISH_VERSION}"
+      
+      cd $packageDir
+      npm publish --access public $TAG
+      cd ../..
+    else
+      echo "Skipping ${PACKAGE_NAME}"
+    fi
+  fi
+done
