bf8b1b32d776b55cd3948ae8b1f804ee296121a8
Ben <br@massa.net>
2023-06-29 09:39:43 +0200

    add dist version job

---
+++ scripts/add_versions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+npm dist-tag add "sc-project-initializer@0.3.0" buildnet
+npm dist-tag add "sc-project-initializer@0.5.0" testnet
+npm dist-tag add "sc-deployer@0.2.0" buildnet
+npm dist-tag add "sc-deployer@0.4.0" testnet
