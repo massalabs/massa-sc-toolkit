d6cf4c78acfde9bf5eab9e46f2ef167458b827f8
Ben <br@massa.net>
2023-06-29 09:51:17 +0200

    fix

--- scripts/add_versions.sh
+++ scripts/add_versions.sh
@@ -3,5 +3,5 @@ set -ex
 
 npm dist-tag add "@massalabs/sc-project-initializer@0.3.0" buildnet
 npm dist-tag add "@massalabs/sc-project-initializer@0.5.0" testnet
-npm dist-tag add "@massalabs/sc-deployer@0.2.0" buildnet
-npm dist-tag add "@massalabs/sc-deployer@0.4.0" testnet
+npm dist-tag add "@massalabs/massa-sc-deployer@0.2.0" buildnet
+npm dist-tag add "@massalabs/massa-sc-deployer@0.4.0" testnet
