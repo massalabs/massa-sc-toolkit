383e0589759ea498d404d091d3d50a19d180d924
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-29 15:24:37 +0200

    Create massa-proto-cli.spec.ts

---
+++ test/massa-proto-cli.spec.ts
@@ -0,0 +1,27 @@
+test('test commands', () => {
+    // TODO: test commands
+    expect(true).toBe(true);
+});
+
+/** 
+ * extract all the necessary information from a proto file
+ * (uses getProtoFunction from packages\massa-proto-cli\src\protobuf.ts)
+ */
+test('test protobuf.ts: getProtoFunction', () => {
+    // TODO: test getProtoFunction
+});
+
+/**
+ * from a proto file, generate the ts helper file
+ */
+test('test ts-gen: compileProtoToTSHelper', () => {
+    //TODO: test compileProtoToTSHelper
+});
+
+/**
+ * from a proto file, generate the ts helper and caller files
+ */
+test('test ts-gen: generateTSCaller', () => {
+    //TODO: test generateTSCaller
+});
+  
