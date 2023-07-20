569a983532cb92bd2fb997e78819000615f96d21
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2023-07-20 14:48:37 +0200

    update version (#296)

--- packages/sc-project-initializer/commands/init/src/deploy.ts
+++ packages/sc-project-initializer/commands/init/src/deploy.ts
@@ -46,6 +46,7 @@ const __dirname = path.dirname(path.dirname(__filename));
         data: readFileSync(path.join(__dirname, 'build', 'main.wasm')), // smart contract bytecode
         coins: fromMAS(0.1), // coins for deployment
         args: new Args().addString('Test'), // arguments for deployment
+        protoPaths: [], // proto files for deployment
       } as ISCData,
       // Additional smart contracts can be added here for deployment
     ],
