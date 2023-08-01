d6132895142f93580de98f51a36b170c03c31549
Ben <br@massa.net>
2023-08-01 09:58:51 +0200

    remove testnet dist tag

--- scripts/add_versions.sh
+++ scripts/add_versions.sh
@@ -4,5 +4,5 @@ set -ex
 # npm dist-tag add "@massalabs/sc-project-initializer@0.3.0" buildnet
 # npm dist-tag add "@massalabs/massa-sc-deployer@0.2.0" buildnet
 
-npm dist-tag rm "@massalabs/sc-project-initializer@0.5.0"
-npm dist-tag rm "@massalabs/massa-sc-deployer@0.4.0"
+npm dist-tag rm "@massalabs/sc-project-initializer@0.5.0" testnet
+npm dist-tag rm "@massalabs/massa-sc-deployer@0.4.0" testnet
