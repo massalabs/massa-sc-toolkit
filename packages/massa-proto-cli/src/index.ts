c89334f7182e4198996bd3fcb6f27fbe63f8ba73
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-08-18 09:53:23 +0200

    Install automatically the necessary dependencies  (#346)
    * Update index.ts
    
    * auto install missing dependencies
    
    * add @protobuf-ts/plugin import into project initializer
    
    * fix ci
    
    * fix ci

--- packages/massa-proto-cli/src/index.ts
+++ packages/massa-proto-cli/src/index.ts
@@ -8,6 +8,7 @@ import { MassaCustomType, extractTypes } from '@massalabs/as-transformer';
 import * as dotenv from 'dotenv';
 import { existsSync, mkdirSync } from 'fs';
 import path from 'path';
+import { exec, execSync } from 'child_process';
 
 // Load .env file content into process.env
 dotenv.config();
@@ -29,7 +30,7 @@ program
     'optional output directory for the callers to generate',
     './helpers/',
   )
-  .parse();
+  .parse(process.argv);
 
 // Get the URL for a public JSON RPC API endpoint from the environment variables
 const publicApi = process.env['JSON_RPC_URL_PUBLIC'];
@@ -54,6 +55,51 @@ async function run() {
     return;
   }
 
+  // check if the necessary dependencies are installed
+  let missingDeps: string[] = [];
+  if (mode === 'sc') {
+    /* check if the following node dependencies are installed: 
+      - as-proto 
+      - as-proto-gen 
+      - @massalabs/massa-as-sdk 
+      - @massalabs/as-types 
+      - @massalabs/as-transformer
+    */
+    const deps = [
+      'as-proto',
+      'as-proto-gen',
+      '@massalabs/massa-as-sdk',
+      '@massalabs/as-types',
+      '@massalabs/as-transformer',
+    ];
+    try {
+      missingDeps = await missingDependencies(deps);
+    } catch (e) {
+      throw new Error(`Error checking for dependencies: ${e}`);
+    }
+  } else if (mode === 'web3' || mode === 'wallet') {
+    /* check if the following node dependencies are installed:
+      - @massalabs/massa-web3
+      - @protobuf-ts/plugin
+    */
+    const deps = ['@massalabs/massa-web3', '@protobuf-ts/plugin'];
+    try {
+      missingDeps = await missingDependencies(deps);
+    } catch (e) {
+      throw new Error(`Error checking for dependencies: ${e}`);
+    }
+  }
+  if (missingDeps.length > 0) {
+    /* eslint-disable-next-line no-console */
+    console.log(`Missing dependencies: ${missingDeps.join(', ')}`);
+
+    for (const dep of missingDeps) {
+      /* eslint-disable-next-line no-console */
+      console.log(`Installing dependency ${dep}...`);
+      installDependency(dep);
+    }
+  }
+
   // execute 'mkdir helpers' if the folder doesn't exist yet
   if (!existsSync(out)) {
     mkdirSync(out);
@@ -76,6 +122,7 @@ async function run() {
     out,
     publicApi,
   );
+  /* eslint-disable-next-line no-console */
   console.warn(
     `For now, we are only using the following custom types because the fetching as issues: u128, u256`,
   );
@@ -107,6 +154,36 @@ async function run() {
     generateTsCallers(files, out, address, mode, address.slice(-10));
   } else {
     throw new Error(`Unsupported mode: ${mode}`);
+  }
+}
+
+async function missingDependencies(dependencies: string[]): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    exec('npm list --depth=0', (error, stdout) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      const notInstalled: string[] = [];
+      for (const dependency of dependencies) {
+        // check if dependency contains / and replace it with \/
+        const regexDependency = dependency.replace('/', '\\/');
+
+        const regex = new RegExp(`${regexDependency}`, 'i');
+        if (!regex.test(stdout)) {
+          notInstalled.push(dependency);
+        }
+      }
+      resolve(notInstalled);
+    });
+  });
+}
+
+function installDependency(dep: string) {
+  try {
+    execSync(`npm install ${dep}`);
+  } catch (e) {
+    throw new Error(`Error installing dependency ${dep}: ${e}`);
   }
 }
 
