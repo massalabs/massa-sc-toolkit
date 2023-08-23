76b7c5960221d41a50135e8f30dab7d308de2161
Ben <br@massa.net>
2023-08-23 13:48:01 +0200

    [massa-proto-cli] release

--- scripts/add_versions.sh
+++ scripts/add_versions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -ex
 
+npm publish "@massalabs/massa-proto-cli@0.0.1"
+
 # npm dist-tag add "@massalabs/sc-project-initializer@0.3.0" buildnet
 # npm dist-tag add "@massalabs/massa-sc-deployer@0.2.0" buildnet
 
-npm dist-tag rm "@massalabs/sc-project-initializer@0.5.0" testnet
-npm dist-tag rm "@massalabs/massa-sc-deployer@0.4.0" testnet
+# npm dist-tag rm "@massalabs/sc-project-initializer@0.5.0" testnet
+# npm dist-tag rm "@massalabs/massa-sc-deployer@0.4.0" testnet
