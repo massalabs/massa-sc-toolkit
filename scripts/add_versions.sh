f5ed635aba1e4f2463f416b39a01643bfe8d375a
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2024-01-02 10:35:47 +0100

    Update buildnet versions

--- scripts/add_versions.sh
+++ scripts/add_versions.sh
@@ -5,8 +5,8 @@ npm publish --access public
 # npm publish "@massalabs/massa-proto-cli"
 # npm publish "@massalabs/massa-sc-deployer" 
 
-# npm dist-tag add "@massalabs/sc-project-initializer@0.3.0" buildnet
-# npm dist-tag add "@massalabs/massa-sc-deployer@1.1.1" latest
+npm dist-tag add "@massalabs/sc-project-initializer@1.3.0" buildnet
+npm dist-tag add "@massalabs/massa-sc-deployer@1.3.0" buildnet
 
 # npm dist-tag rm "@massalabs/sc-project-initializer@0.5.0" testnet
 # npm dist-tag rm "@massalabs/massa-sc-deployer@0.4.0" testnet
