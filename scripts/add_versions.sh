f7087620b1444c5221ccf7823ae8b303dcfc8b14
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2024-01-04 13:46:32 +0100

    Add buildnet tag

--- scripts/add_versions.sh
+++ scripts/add_versions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 cd ./packages/sc-deployer
-npm publish --access public
+# npm publish --access public
 # npm publish "@massalabs/massa-proto-cli"
 # npm publish "@massalabs/massa-sc-deployer" 
 
 npm dist-tag add "@massalabs/sc-project-initializer@1.3.0" buildnet
 npm dist-tag add "@massalabs/massa-sc-deployer@1.3.0" buildnet
 
-# npm dist-tag rm "@massalabs/sc-project-initializer@0.5.0" testnet
-# npm dist-tag rm "@massalabs/massa-sc-deployer@0.4.0" testnet
+npm dist-tag rm "@massalabs/sc-project-initializer@0.9.1-dev.20231121150119" buildnet-dev
+npm dist-tag rm "@massalabs/massa-sc-deployer@0.8.1-dev.20231121150119" buildnet-dev
 
