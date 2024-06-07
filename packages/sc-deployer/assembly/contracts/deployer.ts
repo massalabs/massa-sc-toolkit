e65fb0ed08e39d6edf2eadc89d936018cefaf71d
BenRey <br@massa.net>
2024-06-07 09:57:22 +0200

    Improve deployer readability

--- packages/sc-deployer/assembly/contracts/deployer.ts
+++ packages/sc-deployer/assembly/contracts/deployer.ts
@@ -5,6 +5,7 @@ import {
   call,
   functionExists,
   hasOpKey,
+  generateRawEvent,
 } from '@massalabs/massa-as-sdk';
 import { Args } from '@massalabs/as-types';
 
@@ -18,47 +19,71 @@ const CONSTRUCTOR = 'constructor';
  *
  * @param _ - not used
  */
-export function main(_: StaticArray<u8>): StaticArray<u8> {
-  let masterKey = new StaticArray<u8>(1);
-  masterKey[0] = 0x00;
+export function main(_: StaticArray<u8>): void {
+  let nbSC = getNbSC();
 
-  if (!hasOpKey(masterKey)) {
-    return [];
-  }
-  let nbSCSer = getOpData(masterKey);
-  let nbSC = new Args(nbSCSer).nextU64().unwrap();
+  const deployedSC: string[] = [];
+
   for (let i: u64 = 0; i < nbSC; i++) {
-    let keyBaseArgs = new Args().add(i + 1);
-    let keyBaseCoins = new Args().add(i + 1);
-    let key = keyBaseArgs.serialize();
-    if (!hasOpKey(key)) {
-      return [];
-    }
-    let SCBytecode = getOpData(key);
-    const contractAddr = createSC(SCBytecode);
+    const contractAddr = createSC(getScByteCode(i));
 
     if (functionExists(contractAddr, CONSTRUCTOR)) {
-      let argsIdent = new Uint8Array(1);
-      argsIdent[0] = 0x00;
-      let keyArgs = keyBaseArgs.add(argsIdent).serialize();
-      let coinsIdent = new Uint8Array(1);
-      coinsIdent[0] = 0x01;
-      let keyCoins = keyBaseCoins.add(coinsIdent).serialize();
-      let args: Args;
-      if (hasOpKey(keyArgs)) {
-        args = new Args(getOpData(keyArgs));
-      } else {
-        args = new Args();
-      }
-      let coins: u64;
-      if (hasOpKey(keyCoins)) {
-        coins = new Args(getOpData(keyCoins)).nextU64().unwrap();
-      } else {
-        coins = 0;
-      }
-      call(contractAddr, CONSTRUCTOR, args, coins);
+      call(contractAddr, CONSTRUCTOR, getArgs(i), getCoins(i));
     }
+
     generateEvent(`Contract deployed at address: ${contractAddr.toString()}`);
+    deployedSC.push(contractAddr.toString());
   }
-  return [];
+
+  generateRawEvent(new Args().add(deployedSC).serialize());
+}
+
+function getNbSC(): u64 {
+  let nbSCKey = new StaticArray<u8>(1);
+  nbSCKey[0] = 0x00;
+  assert(hasOpKey(nbSCKey), 'The number of smart contract is not defined.');
+  const nbSCSer = getOpData(nbSCKey);
+  return new Args(nbSCSer).mustNext<u64>('nbSC');
+}
+
+function getScByteCode(i: u64): StaticArray<u8> {
+  const keyScByteCode = new Args().add(i + 1).serialize();
+  // What happen if fail for the second contract, is the first one deployed ?
+  assert(
+    hasOpKey(keyScByteCode),
+    `No bytecode found for contract number: ${i + 1}`,
+  );
+
+  return getOpData(keyScByteCode);
+}
+
+function getKeyArgs(i: u64): StaticArray<u8> {
+  let argsIdentifier = new StaticArray<u8>(1);
+  argsIdentifier[0] = 0x00;
+  return new Args()
+    .add(i + 1)
+    .add(argsIdentifier)
+    .serialize();
+}
+
+function getArgs(i: u64): Args {
+  const keyArgs = getKeyArgs(i);
+  return hasOpKey(keyArgs) ? new Args(getOpData(keyArgs)) : new Args();
+}
+
+function getKeyCoins(i: u64): StaticArray<u8> {
+  let coinsIdentifier = new StaticArray<u8>(1);
+  coinsIdentifier[0] = 0x01;
+  return new Args()
+    .add(i + 1)
+    .add(coinsIdentifier)
+    .serialize();
+}
+
+function getCoins(i: u64): u64 {
+  let keyCoins = getKeyCoins(i);
+
+  return hasOpKey(keyCoins)
+    ? new Args(getOpData(keyCoins)).next<u64>().unwrapOrDefault()
+    : 0;
 }
