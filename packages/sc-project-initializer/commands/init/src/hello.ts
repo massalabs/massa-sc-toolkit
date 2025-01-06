a90671b1fc17f45a2fe9f5e799bddb16dd579bec
BenRey <44082144+Ben-Rey@users.noreply.github.com>
2025-01-06 12:49:59 +0100

    bump version to 2.1.0 and update massa-web3 provider to JsonRpcProvider (#414)

--- packages/sc-project-initializer/commands/init/src/hello.ts
+++ packages/sc-project-initializer/commands/init/src/hello.ts
@@ -1,16 +1,16 @@
 import {
-  Account,
   bytesToStr,
   SmartContract,
-  Web3Provider,
+  JsonRpcProvider,
 } from '@massalabs/massa-web3';
 import 'dotenv/config';
 
 // Paste the address of the deployed contract here
 const CONTRACT_ADDR = 'AS12N5DvTVwvaLbaniMgDJqKwJ3uXBGwzzGuB1f6fjeSx3nhhahTE';
 
-const account = await Account.fromEnv();
-const provider = Web3Provider.buildnet(account);
+// Here we only use the read method of the contract so we don't need an account
+// provider will be a JsonRpcPublicProvider instance
+const provider = JsonRpcProvider.buildnet();
 
 const helloContract = new SmartContract(provider, CONTRACT_ADDR);
 
